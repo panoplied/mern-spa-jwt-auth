4bce0cced77a7204a19a8f3a345c3d6573797f9f
panoplied <panoplied@protonmail.com>
2022-11-29 07:27:27 +0300

    Add hashing passwords when signing up

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,18 @@
+const User = require("../models/userModel");
+
 const login = async (req, res) => {
   res.json({ message: "Login User" });
 };
 
 const signup = async (req, res) => {
-  res.json({ message: "Signup User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10; // determines salt complexity for password hashing
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +16,21 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  const emailExists = await this.findOne({ email });
+  if (emailExists) {
+    throw Error("Email already in use");
+  }
+
+  // Encrypt the password
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hash = await bcrypt.hash(password, salt);
+
+  // Store user credentials
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
