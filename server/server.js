e0a0e066f81298f7a6d17477d5e3db4851d6a136
panoplied <panoplied@protonmail.com>
2022-11-29 03:13:48 +0300

    Set up express server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+// Set up middleware
+app.use((req, res, next) => {
+  // test requests
+  console.log(`Requested ${req.path} with ${req.method} method`);
+  next();
+});
+
+// Set up route
+app.get('/', (req, res) => {
+  res.json({message: 'API is running'});
+});
+
+// Listen for client requests
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
