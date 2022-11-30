f2d8b42387b14f5037c76723b5521f0f4fbf0dbe
panoplied <panoplied@protonmail.com>
2022-11-30 13:19:16 +0300

    Add CORS

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+const userRoutes = require("./routes/user");
+
+const app = express();
+app.use(cors());
+
 const API_PORT = process.env.API_PORT;
 
-// Test Mongo connection
 const { MONGO_HOST, MONGO_USER, MONGO_PASS, MONGO_DB_NAME, MONGO_PORT } =
   process.env;
 const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`;
@@ -10,22 +16,11 @@ mongoose
   .connect(MONGO_URI)
   .then(console.log("connected to mongo"))
   .catch((err) => console.log(err));
-// ---
-
-const express = require("express");
-const app = express();
-const userRoutes = require("./routes/user");
 
 app.use(express.json());
 app.use((req, res, next) => {
   next();
 });
-
-// Test server response
-app.get("/", (req, res) => {
-  res.json({ mssg: "ok" });
-});
-// ---
 
 app.use("/api/user", userRoutes);
 
