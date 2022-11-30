af055a8470162ff2d2b000c1acfc38348cdfee3e
panoplied <panoplied@protonmail.com>
2022-11-30 11:55:53 +0300

    Scaffold signup/login forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    // TODO implement actual login request
+    console.log("DEBUG | LOGIN | ", email, password);
+  };
+
   return (
     <>
       <Link to="/">Home</Link>
       <h1>Login Page</h1>
+      <form onSubmit={submitHandler}>
+        <label>Email</label>
+        <input
+          type="email"
+          onChange={emailChangeHandler}
+          value={email}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={passwordChangeHandler}
+          value={password}
+        />
+      </form>
     </>
   );
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    // TODO implement actual signup request
+    console.log("DEBUG | SIGNUP | ", email, password);
+  };
+
   return (
     <>
       <Link to="/">Home</Link>
       <h1>Signup Page</h1>
+      <form onSubmit={submitHandler}>
+        <label>Email</label>
+        <input
+          type="email"
+          onChange={emailChangeHandler}
+          value={email}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={passwordChangeHandler}
+          value={password}
+        />
+        <button>Signup</button>
+      </form>
     </>
   );
 };
