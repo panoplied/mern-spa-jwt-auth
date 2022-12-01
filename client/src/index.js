cb80e2f2adfffcaa47a121338fba423aef2c63db
panoplied <panoplied@protonmail.com>
2022-12-01 06:04:38 +0300

    Add fonts and basic styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Components
 import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
+
+// Resources
+import './index.css';
+import './fonts/Glass_TTY_VT220.ttf';
+import './fonts/Orden_Regular.woff2';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
