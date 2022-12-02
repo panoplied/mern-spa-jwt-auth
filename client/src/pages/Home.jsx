4602402d262494d4e466b1d5d17d672792c432fa
panoplied <panoplied@protonmail.com>
2022-12-02 06:19:10 +0300

    Add 3D rendered animation of rotating 🌲 cone

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import { useAuthContext } from "../hooks/useAuthContext";
 import { Link } from "react-router-dom";
 
+// Components
 import UserProfile from "../components/UserProfile";
 import Scanlines from "../components/Scanlines";
+
+// Resources
+import ConeVideo from "../video/cone1440.webm";
 
 const Home = () => {
   const { user } = useAuthContext();
@@ -19,6 +23,11 @@ const Home = () => {
           <Link to="/signup">Sign Up</Link>
         </div>
       )}
+
+      <video autoPlay muted loop>
+        <source src={ConeVideo} type="video/webm" />
+      </video>
+
     </>
   );
 };
