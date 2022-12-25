e1d1907a3a3c2235eb326a9f2718c839b4527edc
panoplied <panoplied@protonmail.com>
2022-12-25 20:28:13 +0300

    Simplify pass reqs

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,18 +5,11 @@ const validator = require("validator");
 const SALT_ROUNDS = 10; // determines salt complexity for password hashing
 
 const PASS_REQUIREMENTS = {
-  minLength: 8,
+  minLength: 6,
   minLowercase: 1,
   minUppercase: 1,
   minNumbers: 1,
-  minSymbols: 1,
-  returnScore: false,
-  pointsPerUnique: 1,
-  pointsPerRepeat: 0.5,
-  pointsForContainingLower: 10,
-  pointsForContainingUpper: 10,
-  pointsForContainingNumber: 10,
-  pointsForContainingSymbol: 10,
+  minSymbols: 0,
 };
 
 const Schema = mongoose.Schema;
